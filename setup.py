238398164de5eb4bd96b71044411b9cfe6bf542c
jakeogh <github.com@v6y.net>
2025-08-14 00:15:55 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from setuptools import find_packages
+from setuptools import setup
+
 import fastentrypoints
-from setuptools import find_packages, setup
 
 dependencies = ["click"]
 
@@ -14,15 +16,15 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "read stats from the Kimball Physics igps-1101a ion gun power supply",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"power_supply_igps1101a_tool": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"power_supply_igps1101a_tool": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
     "install_requires": dependencies,
     "entry_points": {
         "console_scripts": [
-            "power-supply-igps1101a-tool=power_supply_igps1101a_tool.power_supply_igps1101a_tool:cli",
+            "power-supply-igps1101a-tool=power_supply_igps1101a_tool.cli:cli",
         ],
     },
 }
