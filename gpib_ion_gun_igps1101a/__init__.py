f9fa360fa1d580e9c998667d61229222ba4be2dd
jakeogh <github.com@v6y.net>
2025-09-11 17:03:38 -0700

    auto-commit

--- gpib_ion_gun_igps1101a/__init__.py
+++ gpib_ion_gun_igps1101a/__init__.py
@@ -2,4 +2,4 @@
 isort:skip_file
 """
 
-from .gpib_ion_gun_igps1101a import gpib_ion_gun_igps1101a as gpib_ion_gun_igps1101a
+# from .gpib_ion_gun_igps1101a import gpib_ion_gun_igps1101a as gpib_ion_gun_igps1101a
